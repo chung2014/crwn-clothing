b5a2a0c39c08a8c3b08d66956cce1e4803c0ab5a
chung2014 <chung2014@gmail.com>
2020-06-27 10:08:16 +0000

    styled component but failed to connect firebase in cu lib computer

---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HomePageContainer = styled.div`
+    background-color: yellowgreen;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
